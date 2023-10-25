f2a781ec7ad3d36fbbdca23d34e2e45bec77c843
Gary Miguel <garymm@garymm.org>
2023-10-25 16:57:54 -0700

    it runs!

--- rl/1_simple_pg.py
+++ rl/1_simple_pg.py
@@ -1,11 +1,8 @@
 # Based on https://github.com/openai/spinningup/blob/038665d62d569055401d91856abb287263096178/spinup/examples/pytorch/pg_math/1_simple_pg.py
-import torch
-import torch.nn as nn
-from torch.distributions.categorical import Categorical
-from torch.optim import Adam
-import numpy as np
-import gym
-from gym.spaces import Discrete, Box
+import jax.numpy as np
+import gymnasium
+from gymnasium.spaces import Discrete, Box
+from jaxtyping import Array, Float, Int, PyTree  # https://github.com/google/jaxtyping
 
 
 def train(
@@ -17,7 +14,7 @@ def train(
     render=False,
 ):
     # make environment, check spaces, get obs / act dims
-    env = gym.make(env_name)
+    env = gymnasium.make(env_name, render_mode="human")
     assert isinstance(
         env.observation_space, Box
     ), "This example only works for envs with continuous state spaces."
@@ -37,8 +34,8 @@ def train(
         pass
 
     # make action selection function (outputs int actions, sampled from policy)
-    def get_action(obs):
-        pass  # TODO
+    def get_action(obs: Float[Array, str(obs_dim)]):
+        return 0  # TODO
 
     # make loss function whose gradient, for the right data, is policy gradient
     def compute_loss(obs, act, weights):
@@ -58,7 +55,7 @@ def train(
         batch_lens = []  # for measuring episode lengths
 
         # reset episode-specific variables
-        obs = env.reset()  # first obs comes from starting distribution
+        obs = env.reset()[0]  # first obs comes from starting distribution
         done = False  # signal from environment that episode is over
         ep_rews = []  # list for rewards accrued throughout ep
 
@@ -75,8 +72,8 @@ def train(
             batch_obs.append(obs.copy())
 
             # act in the environment
-            act = get_action(torch.as_tensor(obs, dtype=torch.float32))
-            obs, rew, done, _ = env.step(act)
+            act = get_action(np.asarray(obs, dtype=np.float32))
+            obs, rew, done, _, _ = env.step(act)
 
             # save action, reward
             batch_acts.append(act)
@@ -92,7 +89,7 @@ def train(
                 batch_weights += [ep_ret] * ep_len
 
                 # reset episode-specific variables
-                obs, done, ep_rews = env.reset(), False, []
+                (obs, _), done, ep_rews = env.reset(), False, []
 
                 # won't render again this epoch
                 finished_rendering_this_epoch = True
@@ -102,30 +99,30 @@ def train(
                     break
 
         # take a single policy gradient update step
-        optimizer.zero_grad()
+        # optimizer.zero_grad()
         batch_loss = compute_loss(
-            obs=torch.as_tensor(batch_obs, dtype=torch.float32),
-            act=torch.as_tensor(batch_acts, dtype=torch.int32),
-            weights=torch.as_tensor(batch_weights, dtype=torch.float32),
+            obs=np.asarray(batch_obs, dtype=np.float32),
+            act=np.asarray(batch_acts, dtype=np.int32),
+            weights=np.asarray(batch_weights, dtype=np.float32),
         )
-        batch_loss.backward()
-        optimizer.step()
-        return batch_loss, batch_rets, batch_lens
+        # batch_loss.backward()
+        # optimizer.step()
+        return batch_loss, np.asarray(batch_rets), np.asarray(batch_lens)
 
     # training loop
     for i in range(epochs):
         batch_loss, batch_rets, batch_lens = train_one_epoch()
-        print(
-            "epoch: %3d \t loss: %.3f \t return: %.3f \t ep_len: %.3f"
-            % (i, batch_loss, np.mean(batch_rets), np.mean(batch_lens))
-        )
+        # print(
+        #     "epoch: %3d \t loss: %.3f \t return: %.3f \t ep_len: %.3f"
+        #     % (i, batch_loss, np.mean(batch_rets), np.mean(batch_lens))
+        # )
 
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--env_name", "--env", type=str, default="CartPole-v0")
+    parser.add_argument("--env_name", "--env", type=str, default="CartPole-v1")
     parser.add_argument("--render", action="store_true")
     parser.add_argument("--lr", type=float, default=1e-2)
     args = parser.parse_args()
