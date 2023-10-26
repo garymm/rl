8b6ee79a672110198bcd859f6b2067abae124029
Gary Miguel <garymm@garymm.org>
2023-10-26 16:16:49 -0700

    It is working! I had to split the random key to get different actions sampled from my network.

--- rl/1_simple_pg.py
+++ rl/1_simple_pg.py
@@ -29,7 +29,7 @@ def compute_loss(
     """loss function whose gradient, for the right data, is policy gradient"""
     logits = jax.vmap(logits_net)(obs)
     log_probs = jax.vmap(jax.nn.log_softmax)(logits)
-    log_probs_for_actions = log_probs[:, acts]
+    log_probs_for_actions = log_probs[np.arange(log_probs.shape[0]), acts]
     return -np.mean(weights * log_probs_for_actions)
 
 
@@ -38,7 +38,7 @@ def train(
     render: bool,
     lr: float,
     hidden_sizes=[32],
-    epochs=500,
+    epochs=50,
     batch_size=5000,
 ):
     # make environment, check spaces, get obs / act dims
@@ -54,15 +54,15 @@ def train(
     n_acts = env.action_space.n
 
     # make core of policy network
-    prng_key = jax.random.key(0)
-    logits_net = make_mlp([obs_dim] + hidden_sizes + [n_acts], prng_key)
+    logits_net = make_mlp([obs_dim] + hidden_sizes + [n_acts], jax.random.key(0))
 
     # make action selection function (outputs int actions, sampled from policy)
-    def get_action(model: eqx.Module, obs: Float[Array, str(obs_dim)]) -> int:
-        return jax.random.categorical(prng_key, model(obs)).item()
+    def get_action(model: eqx.Module, obs: Float[Array, str(obs_dim)], prng_key) -> int:
+        logits = model(obs)
+        return jax.random.categorical(prng_key, logits).item()
 
     # make optimizer
-    optimizer = optax.adamw(lr)
+    optimizer = optax.adam(lr)
     opt_state = optimizer.init(eqx.filter(logits_net, eqx.is_array))
 
     surface = None
@@ -70,13 +70,13 @@ def train(
         env.reset()
         pixels = env.render().transpose(1, 0, 2)
         pygame.init()
-        surface = pygame.display.set_mode(
-            (pixels.shape[0], pixels.shape[1]), pygame.DOUBLEBUF
-        )
+        surface = pygame.display.set_mode((pixels.shape[0], pixels.shape[1]))
 
     # for training policy
     # TODO: @eqx.filter_jit
-    def train_one_epoch(model: eqx.Module, opt_state: optax.OptState) -> tuple[float]:
+    def train_one_epoch(
+        model: eqx.Module, opt_state: optax.OptState, prng_key: Array
+    ) -> tuple:
         # make some empty lists for logging.
         batch_obs = []  # for observations
         batch_acts = []  # for actions
@@ -104,7 +104,8 @@ def train(
             batch_obs.append(obs.copy())
 
             # act in the environment
-            act = get_action(model, np.asarray(obs, dtype=np.float32))
+            prng_key, subkey = jax.random.split(prng_key)
+            act = get_action(model, np.asarray(obs, dtype=np.float32), subkey)
             obs, rew, done, _, _ = env.step(act)
 
             # save action, reward
@@ -141,13 +142,19 @@ def train(
         model = eqx.apply_updates(model, updates)
         return model, opt_state, batch_loss, np.asarray(batch_rets), np.asarray(
             batch_lens
-        )
+        ), prng_key
 
     # training loop
+    prng_key = jax.random.key(0)
     for i in range(epochs):
-        logits_net, opt_state, batch_loss, batch_rets, batch_lens = train_one_epoch(
-            logits_net, opt_state
-        )
+        (
+            logits_net,
+            opt_state,
+            batch_loss,
+            batch_rets,
+            batch_lens,
+            prng_key,
+        ) = train_one_epoch(logits_net, opt_state, prng_key)
         print(
             "epoch: %3d \t loss: %.3f \t return: %.3f \t ep_len: %.3f"
             % (i, batch_loss, np.mean(batch_rets), np.mean(batch_lens))
